1437583190b8631e95249588787536cc203f34d9
L. Maden <lm859@cam.ac.uk>
2025-06-11 16:18:53 +0100

    Class to describe annulus lines and interpolate coordinates

--- blading/blade.py
+++ blading/blade.py
@@ -1,8 +1,12 @@
-from dataclasses import dataclass
-from typing import Callable, Optional, Self
+from geometry.surfaces import plot_2d_surface
+from dataclasses import dataclass, field
+from typing import Callable, Generic, Mapping, MutableMapping, Optional, Self, TypeVar
 from geometry.curves import PlaneCurve
+from geometry.grid import Grid2D
 from numpy.typing import NDArray
 import numpy as np
+from scipy.interpolate import griddata
+from scipy.optimize import minimize
 
 
 @dataclass(frozen=True)
@@ -48,25 +52,73 @@ class Section:
         return normal * thickness
 
 
+T = TypeVar("T", bound=MutableMapping)
+
+
 @dataclass(frozen=True)
-class PSection:
-    params: dict[str, float]
-    create: Callable[[dict[str, float]], Section]
+class PSection(Generic[T]):
+    params: T
+    create: Callable[[T], Section]
 
     def section(self) -> Section:
         return self.create(self.params)
 
-    def update_params(self, params: dict[str, float]) -> "PSection":
-        new_params = dict(self.params, **params)
-        return PSection(new_params, self.create)
+    def update(self, params: T) -> "PSection":
+        updated = type(params)()
+        updated.update(**self.params)  # Fill with old parameters
+        updated.update(params)  # Update new parameters
+        return PSection(updated, self.create)
 
 
 @dataclass
 class PBlade:
     sections: list[PSection]
 
-    def span(self) -> None: ...
 
+@dataclass(frozen=True)
+class Annulus:
+    hub: PlaneCurve
+    cas: PlaneCurve
+    point_distr: Optional[NDArray] = None
 
-def param_spline() -> None:
-    pass
+    _interp_stream: NDArray = field(init=False)
+    _interp_physical: NDArray = field(init=False)
+
+    def __post_init__(self) -> None:
+        object.__setattr__(self, "hub", self.hub.reparameterise_unit().normalise())
+        object.__setattr__(self, "cas", self.cas.reparameterise_unit().normalise())
+        self._create_interp_grid()
+
+    def _calc_point(self, coord):
+        xi, eta = coord
+        hub = self.hub.interpolate([xi])
+        cas = self.cas.interpolate([xi])
+        return hub * (1 - eta) + cas * eta
+
+    def physical_coords(self, stream_coords: NDArray) -> NDArray:
+        streamwise, spanwise = stream_coords.T
+        hub = self.hub.interpolate(streamwise)
+        cas = self.cas.interpolate(streamwise)
+        k = spanwise[:, np.newaxis]
+        return hub.coords * (1 - k) + cas.coords * k
+
+    def stream_coords(self, physical_coords: NDArray) -> NDArray:
+        streamwise_grid = self._interp_stream[:, 0]
+        spanwise_grid = self._interp_stream[:, 1]
+
+        streamwise = griddata(self._interp_physical, streamwise_grid, physical_coords)
+        spanwise = griddata(self._interp_physical, spanwise_grid, physical_coords)
+
+        return np.c_[streamwise, spanwise]
+
+    def _create_interp_grid(self) -> None:
+        N = 100
+        pts = np.linspace(-0.1, 1.1, N)
+        streamwise_grid, spanwise_grid = np.meshgrid(pts, pts, indexing="ij")
+        stream_grid = np.stack((streamwise_grid, spanwise_grid), axis=2)
+        grid_coords = np.zeros((N, N, 2))
+        for i in range(N):
+            grid_coords[i] = self.physical_coords(stream_grid[i])
+
+        object.__setattr__(self, "_interp_physical", grid_coords.reshape((-1, 2)))
+        object.__setattr__(self, "_interp_stream", stream_grid.reshape((-1, 2)))
